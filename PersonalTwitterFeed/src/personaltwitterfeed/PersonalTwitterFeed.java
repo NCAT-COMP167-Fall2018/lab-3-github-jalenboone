19fc1523e7e4ee44084cba9ed0374fc57d8c0fbd
Jalen Boone <jhboone@aggies.ncat.edu>
2018-09-19 15:39:17 -0400

    Level 2 of lab 3

--- PersonalTwitterFeed/src/personaltwitterfeed/PersonalTwitterFeed.java
+++ PersonalTwitterFeed/src/personaltwitterfeed/PersonalTwitterFeed.java
@@ -33,24 +33,10 @@ public class PersonalTwitterFeed {
         System.out.println("Enter your tweets and I will add them to your timeline!");
         
         int numTweets = 0;
-        
-        while(numTweets < (MAX_NUMBER_TWEETS - 1)) {
-            tweets[numTweets] = keyboard.nextLine();
-            numTweets++;
-            
-            System.out.println(tweeterName + "'s Personal Twitter Feed:");
-            for(int i = 0; i < numTweets; i++) {
-                System.out.println("- " + tweets[i] + getCurrentTimeStamp());
-            }
-            
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            
-            if(numTweets < (MAX_NUMBER_TWEETS - 1))
-                System.out.println("Enter your next tweet:");
+        while(numTweets<(MAX_NUMBER_TWEETS-1)){
+        newTweet(tweets);
         }
+      
         
         System.out.println("Your twitter feed is full");
     }
@@ -71,5 +57,25 @@ public class PersonalTwitterFeed {
     String date = simpleDateFormat.format(new Date());
     return date;
     }
-    
+    private static void newTweet(String[] tweets){
+        int numTweets=0;
+        Scanner keyboard = new Scanner(System.in);
+        String tweeterName = keyboard.nextLine();
+          while(numTweets < (MAX_NUMBER_TWEETS - 1)) {
+            tweets[numTweets] = keyboard.nextLine();
+            numTweets++;
+            
+            System.out.println(tweeterName + "'s Personal Twitter Feed:");
+            for(int i = 0; i < numTweets; i++) {
+                System.out.println("- " + tweets[i] + getCurrentTimeStamp());
+            }
+            
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            
+            if(numTweets < (MAX_NUMBER_TWEETS - 1))
+                System.out.println("Enter your next tweet:");
+        }
+    }
 }
